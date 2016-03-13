d93526c3882f5b56b045cb0a310f8eb4a437df3c
刀哥 <liufan321@gmail.com>
2016-03-13 14:32:57 +0800

    添加单图查看控制器／设置状态栏显示

--- HMPhotoBrowser/PhotoBrowser/HMPhotoBrowserController.m
+++ HMPhotoBrowser/PhotoBrowser/HMPhotoBrowserController.m
@@ -8,6 +8,7 @@
 
 #import "HMPhotoBrowserController.h"
 #import "HMPhotoBrowserPhotos.h"
+#import "HMPhotoViewerController.h"
 
 @interface HMPhotoBrowserController ()
 
@@ -15,10 +16,10 @@
 
 @implementation HMPhotoBrowserController {
     HMPhotoBrowserPhotos *_photos;
+    BOOL _statusBarHidden;
 }
 
 #pragma mark - 构造函数
-
 + (instancetype)photoBrowserWithSelectedIndex:(NSInteger)selectedIndex urls:(NSArray<NSString *> *)urls parentImageViews:(NSArray<UIImageView *> *)parentImageViews {
     return [[self alloc] initWithSelectedIndex:selectedIndex
                                           urls:urls
@@ -34,15 +35,60 @@
         _photos.selectedIndex = selectedIndex;
         _photos.urls = urls;
         _photos.parentImageViews = parentImageViews;
+        
+        _statusBarHidden = NO;
     }
     return self;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    [self prepareUI];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
     
-    self.view.backgroundColor = [UIColor whiteColor];
-    NSLog(@"%@", _photos);
+    _statusBarHidden = YES;
+    [UIView animateWithDuration:0.25 animations:^{
+        [self setNeedsStatusBarAppearanceUpdate];
+    }];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    
+    _statusBarHidden = NO;
+    [self setNeedsStatusBarAppearanceUpdate];
+}
+
+- (UIStatusBarAnimation)preferredStatusBarUpdateAnimation {
+    return UIStatusBarAnimationSlide;
+}
+
+- (BOOL)prefersStatusBarHidden {
+    return _statusBarHidden;
+}
+
+#pragma mark - 监听方法
+- (void)tapGesture {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - 设置界面
+/// 设置界面
+- (void)prepareUI {
+    self.view.backgroundColor = [UIColor orangeColor];
+
+    HMPhotoViewerController *viewer = [[HMPhotoViewerController alloc] init];
+    
+    [self.view addSubview:viewer.view];
+    [self addChildViewController:viewer];
+    [viewer didMoveToParentViewController:self];
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapGesture)];
+    [self.view addGestureRecognizer:tap];
 }
 
 @end
